9ec73626d0a72267b0043084e5e7c6c7ef585653
duyng404 <4320964+duyng404@users.noreply.github.com>
2019-01-21 00:12:12 -0600

    wait for db on startup

--- gorm/gorm.go
+++ gorm/gorm.go
@@ -11,8 +11,6 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql" // mysql
 )
 
-var dbRetryAttempts = 10
-
 var (
 	// DB : the connection object for the db
 	DB *gorm.DB
@@ -29,21 +27,14 @@ func InitDB() (*gorm.DB, error) {
 	var db *gorm.DB
 	var err error
 
-	for true {
-		// Create the Url used to Open the db
-		dbURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", config.DBUsername, config.DBPassword, config.DBHostname, config.DBPort, config.DBName)
-		logger.Info("dburl is:", dbURL)
-		logger.Info("Opening a connection to the db...")
-		db, err = gorm.Open("mysql", dbURL)
-		if err != nil {
-			if dbRetryAttempts == 0 {
-				logger.Info("Couldn't open a connection to the db!", err)
-				logger.Info("Too many tries! Giving up!")
-				return nil, err
-			}
-			dbRetryAttempts--
-		}
-		break
+	// Create the Url used to Open the db
+	dbURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", config.DBUsername, config.DBPassword, config.DBHostname, config.DBPort, config.DBName)
+	logger.Info("dburl is:", dbURL)
+	logger.Info("Opening a connection to the db...")
+	db, err = gorm.Open("mysql", dbURL)
+	if err != nil {
+		logger.Info("Couldn't open a connection to the db!", err)
+		return nil, err
 	}
 
 	// gorm's logging is super f-ing annoying like wtf man why
