f349afe105efa76e7f7d0a2cd6e3a0c4a0176046
duyng404 <4320964+duyng404@users.noreply.github.com>
2019-01-22 11:28:43 -0600

    cors only allow admindash

--- gin/routes.go
+++ gin/routes.go
@@ -14,7 +14,7 @@ var count = 0
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", config.AdminDashboardURL)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
