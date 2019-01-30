a060a00651ea6ecd01b0a3b6a1b596f470294323
duyng404 <4320964+duyng404@users.noreply.github.com>
2019-01-29 23:50:33 -0600

    placed -> prep complete

--- socket/adminQueueHandlers.go
+++ socket/adminQueueHandlers.go
@@ -8,7 +8,7 @@ import (
 // commit queue means move from queue to prep
 func handleCommitQueue(commited []int) []*gorm.Order {
 	if len(commited) > 0 {
-		logger.Info("processing these commited order IDs from queue:", commited)
+		logger.Info("processing queue -> prep:", commited)
 		processed := []*gorm.Order{}
 		for _, v := range commited {
 			// get order from db
@@ -28,8 +28,35 @@ func handleCommitQueue(commited []int) []*gorm.Order {
 			processed = append(processed, &o)
 		}
 		return processed
-	} else {
-		logger.Info("nothing to process.")
 	}
+	logger.Info("nothing to process.")
+	return []*gorm.Order{}
+}
+
+// commit prep means move from prep to ship
+func handleCommitPrep(commited []int) []*gorm.Order {
+	if len(commited) > 0 {
+		logger.Info("processing prep -> ship:", commited)
+		processed := []*gorm.Order{}
+		for _, v := range commited {
+			// get order from db
+			var o gorm.Order
+			err := o.PopulateByID(uint(v))
+			if err != nil {
+				logger.Error("error getting order from db")
+				return []*gorm.Order{}
+			}
+			// set a new status
+			err = o.SetStatusTo(gorm.OrderStatusShipping)
+			if err != nil {
+				logger.Error("error setting new status")
+				return processed
+			}
+			// accumulate to processed
+			processed = append(processed, &o)
+		}
+		return processed
+	}
+	logger.Info("nothing to process.")
 	return []*gorm.Order{}
 }
