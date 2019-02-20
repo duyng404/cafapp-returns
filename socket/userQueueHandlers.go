1654029456b9a6f462cc1ab11af6d0deaf0860ee
dpham273 <duymosby@gmail.com>
2019-02-20 17:06:16 -0600

    fixed bugs

--- socket/userQueueHandlers.go
+++ socket/userQueueHandlers.go
@@ -2,6 +2,7 @@ package socket
 
 import (
 	"cafapp-returns/gorm"
+	"strconv"
 )
 
 func (c *client) handleChatbotRequest(request string, user *gorm.User) string {
@@ -11,18 +12,18 @@ func (c *client) handleChatbotRequest(request string, user *gorm.User) string {
 	if err != nil {
 		return "You haven't placed any order"
 	}
-	if request == "orders"{
+	if request == "orders" {
 		for i := range orders {
 			if orders[i].StatusCode >= 20 && orders[i].StatusCode < 60 {
 				count++
-				num := string(count)
-				response += orders[i].UUID+", "
-				return "You have "+num+" orders in queue: "+response
-			}  
+				num := strconv.Itoa(count)
+				response += orders[i].Tag + ", "
+				return "You have " + num + " orders in queue: " + response
+			}
 		}
 	}
-	if request == "help"{
+	if request == "help" {
 		return "Type orders for your order's details"
 	}
-	return ""
+	return "command not recognized"
 }
