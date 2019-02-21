65d8f059e5a2e40b07585cbec323cc2e84a15684
Lam Nguyen <lnguye16@gustavus.edu>
2019-02-21 11:10:48 +0700

    fix loi khi call func populateByID

--- gin/handleAdminViewUsers.go
+++ gin/handleAdminViewUsers.go
@@ -4,6 +4,7 @@ import (
 	"cafapp-returns/gorm"
 	"cafapp-returns/logger"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"strconv"
 )
 
@@ -42,16 +43,19 @@ func handleGetUserAndAllOrdersFromUser(c *gin.Context) {
 	var user gorm.User
 	userID, err := strconv.ParseUint(c.Param("userid"), 16, 16)
 	if err != nil {
-		logger.Info("Cannot convert userid to uint")
+		c.Status(http.StatusInternalServerError)
 		return
 	}
 	//get a list of order object for an user
 	allOrders, err := gorm.GetAllOrderFromUser(uint(userID))
-	//get an user object
-	userInfo := user.PopulateByID(uint(userID))
 
+	err1 := user.PopulateByID(uint(userID))
+	if err1 != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
 	c.JSON(200, gin.H{
 		"allOrders": allOrders,
-		"userInfo":  userInfo,
+		"userInfo":  user,
 	})
 }
